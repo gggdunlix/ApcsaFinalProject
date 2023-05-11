7797c6b2471b447fcf340a119a1635f4a34cd6e3
GGG Dunlix <33298379+gggdunlix@users.noreply.github.com>
2023-05-11 09:17:13 -0400

    Update BrowserStuff.java

--- BrowserStuff.java
+++ BrowserStuff.java
@@ -1,9 +1,25 @@
-
+import java.util.*;
 /**
  * Final project, who knows what this does?
 * Joshua Alessi - APCSA
  */
 public class BrowserStuff
 {
-    private 
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome - type help");
+        String nextLine = scan.nextLine();
+        if (nextLine.equals("help")) { helpCmd(); }
+    }
+    public void helpCmd() {
+        
+        System.out.println("--- Help ---");
+        System.out.println("login <username>");
+
+    }
+    public void clearConsole() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println();
+        }
+    }
 }
